07e680d3c9cc0a73617695230be3e2af13a750ce
Erik Muro <erikmuro@Eriks-iMac.local>
2016-12-10 14:07:09 -0600

    works with emitting buffer image to server

--- app.js
+++ app.js
@@ -5,15 +5,16 @@ var cam = new v4l2camera.Camera("/dev/video0");
 console.log(cam.configGet().formatName);
 
 if (cam.configGet().formatName !== "YUYV") {
-    console.log("YUYV camera required");
-    process.exit(1);
+    // console.log("YUYV camera required");
+    // process.exit(1);
 }
 cam.configSet({width: 352, height: 288});
 cam.start();
 cam.capture(function loop() {
     cam.capture(loop);
+    socket.emit('imageBuffer', Buffer(cam.frameRaw()) )
 });
 socket.on('connect', function(){
     socket.emit('chat message', "Hello from camera");
-    socket.emit('imageBuffer', Buffer(cam.frameRaw()) )
+
 });
