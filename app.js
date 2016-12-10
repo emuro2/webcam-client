b43a8fbceebdd147a63dc791c7a27d31e0100b28
Erik Muro <erikmuro@Eriks-iMac.local>
2016-12-10 15:57:52 -0600

    debugging cam streaming

--- app.js
+++ app.js
@@ -2,17 +2,16 @@ var socket = require('socket.io-client')('https://10.0.0.9');
 var v4l2camera = require("v4l2camera");
 var cam = new v4l2camera.Camera("/dev/video0");
 
-console.log(cam.configGet().formatName);
-
 if (cam.configGet().formatName !== "YUYV") {
-    // console.log("YUYV camera required");
-    // process.exit(1);
+    console.log("YUYV camera required");
+    process.exit(1);
 }
-cam.configSet({width: 352, height: 288});
+cam.configSet({width: 240, height: 297});
 cam.start();
 cam.capture(function loop() {
-    cam.capture(loop);
-    socket.emit('imageBuffer', Buffer(cam.frameRaw()) )
+    var frame = cam.frameRaw();
+    socket.emit('imageBuffer', Buffer(frame) );
+    // cam.capture(loop);
 });
 socket.on('connect', function(){
     socket.emit('chat message', "Hello from camera");
